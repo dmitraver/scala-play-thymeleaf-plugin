0c5023a2371c297427e3653cf330ed0d9aaa2aa5
Dmitry Avershin <dmitraver@gmail.com>
2014-10-27 20:59:07 +0600

    plugin skeleton

---
+++ app/ThymeleafPlugin.scala
@@ -0,0 +1,10 @@
+import play.api.Plugin
+
+class ThymeleafPlugin extends Plugin {
+	
+	override def onStart(): Unit = super.onStart()
+
+	override def onStop(): Unit = super.onStop()
+
+	override def enabled: Boolean = super.enabled
+}
