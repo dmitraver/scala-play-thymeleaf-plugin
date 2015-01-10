71cde91009a25284a227edb272762d389b8d4303
Dmitry Avershin <dmitraver@gmail.com>
2015-01-10 22:38:52 +0100

    implemented form wrapper for easier form error processing in templates

---
+++ app/utils/FormWrapper.scala
@@ -0,0 +1,31 @@
+package utils
+
+import play.api.data.Form
+import java.util
+
+class FormWrapper[T] (form: Form[T]) {
+
+	def hasErrors(field: String): Boolean = form.error(field).isDefined
+
+	def errors(field: String) = {
+		val errors = new util.LinkedList[String]()
+		form.errors(field).foreach(formError => errors add formError.message)
+		errors
+	}
+
+	def allErrors() = {
+		val errors = new util.LinkedList[String]()
+		form.errors.foreach(formError => errors add formError.message)
+		errors
+	}
+
+	def globalErrors() = {
+		val errors = new util.LinkedList[String]()
+		form.globalErrors.foreach(formError => errors add formError.message)
+		errors
+	}
+}
+
+object FormWrapper {
+	def apply[T](form: Form[T]): FormWrapper[T] = new FormWrapper[T](form)
+}
