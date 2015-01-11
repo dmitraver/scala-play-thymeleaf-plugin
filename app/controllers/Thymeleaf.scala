4b04f1f4c0d67830eb0a409009ae537b038342ec
Dmitry Avershin <dmitraver@gmail.com>
2015-01-11 16:50:56 +0100

    implemented template resolver configuration properties

--- app/controllers/Thymeleaf.scala
+++ app/controllers/Thymeleaf.scala
@@ -16,13 +16,37 @@ import scala.collection.JavaConversions._
 
 object Thymeleaf {
 
+	private val THYMELEAF_CACHE_ENABLED_PROPERTY_KEY = "thymeleaf.cache.enabled"
+	private val THYMELEAF_CACHE_ENABLED_PROPERTY_DEFAULT_VALUE = true
+
+	private val THYMELEAF_CACHE_TTL_IN_MILLIS_PROPERTY_KEY = "thymeleaf.cache.ttlms"
+	private val THYMELEAF_CACHE_TTL_IN_MILLIS_PROPERTY_DEFAULT_VALUE = null
+
+	private val THYMELEAF_TEMPLATE_MODE_PROPERTY_KEY = "thymeleaf.template.mode"
+	private val THYMELEAF_TEMPLATE_MODE_PROPERTY_DEFAULT_VALUE = "XHTML"
+
+	private val THYMELEAF_TEMPLATE_PREFIX_PROPERTY_KEY = "thymeleaf.template.prefix"
+	private val THYMELEAF_TEMPLATE_PREFIX_PROPERTY_DEFAULT_VALUE = "public"
+
+	private val THYMELEAF_TEMPLATE_SUFFIX_PROPERTY_KEY = "thymeleaf.template.suffix"
+	private val THYMELEAF_TEMPLATE_SUFFIX_PROPERTY_DEFAULT_VALUE = ".html"
+
 	private val templateEngine = new TemplateEngine
 	private val resourceResolver = new PlayResourceResolver
 	private val messageResolver = new PlayMessageResolver
 	private val templateResolver = new PlayTemplateResolver(resourceResolver)
 
 	templateResolver.setCacheable(Play.application.configuration.
-					getBoolean("thymeleaf.cache.enabled", true))
+					getBoolean(THYMELEAF_CACHE_ENABLED_PROPERTY_KEY, THYMELEAF_CACHE_ENABLED_PROPERTY_DEFAULT_VALUE))
+	templateResolver.setCacheTTLMs(Play.application.configuration.
+					getLong(THYMELEAF_CACHE_TTL_IN_MILLIS_PROPERTY_KEY, THYMELEAF_CACHE_TTL_IN_MILLIS_PROPERTY_DEFAULT_VALUE))
+	templateResolver.setTemplateMode(Play.application.configuration.
+					getString(THYMELEAF_TEMPLATE_MODE_PROPERTY_KEY, THYMELEAF_TEMPLATE_MODE_PROPERTY_DEFAULT_VALUE))
+	templateResolver.setPrefix(Play.application.configuration.
+					getString(THYMELEAF_TEMPLATE_PREFIX_PROPERTY_KEY, THYMELEAF_TEMPLATE_PREFIX_PROPERTY_DEFAULT_VALUE))
+	templateResolver.setSuffix(Play.application.configuration.
+					getString(THYMELEAF_TEMPLATE_SUFFIX_PROPERTY_KEY, THYMELEAF_TEMPLATE_SUFFIX_PROPERTY_DEFAULT_VALUE))
+
 	templateEngine.setTemplateResolver(templateResolver)
 	templateEngine.addDialect(new PlayDialect)
 	templateEngine.setMessageResolver(messageResolver)
@@ -41,7 +65,7 @@ object Thymeleaf {
 				case _ => obj._2
 			}
 
-			templateVariables.put(obj._1, obj)}
+			templateVariables.put(obj._1, templateObject)}
 
 		templateVariables.put("session", SessionMap(session))
 		templateVariables.put("flash", FlashMap(flash))
--- app/template/PlayResourceResolver.scala
+++ app/template/PlayResourceResolver.scala
@@ -9,25 +9,16 @@ import play.{Logger, Play}
 class PlayResourceResolver extends IResourceResolver {
 
 	private val PLAY_RESOURCE_RESOLVER_NAME = "PLAY_RESOURCE_RESOLVER"
-	private val DEFAULT_TEMPLATES_DIRECTORY_PREFIX: String = "public"
-
-	private var prefix = ""
-
-	def setTemplatesDirectoryPrefix(prefix: String) {
-		this.prefix = prefix
-	}
 
 	override def getName: String = PLAY_RESOURCE_RESOLVER_NAME
 
 	override def getResourceAsStream(templateProcessingParameters: TemplateProcessingParameters, resourceName: String): InputStream = {
 		val rootPath: File = Play.application.path
-		val templatesDirectoryPrefix: String = if (!prefix.isEmpty) prefix else DEFAULT_TEMPLATES_DIRECTORY_PREFIX
-		val templateFile: File = new File(rootPath, templatesDirectoryPrefix + "/" + resourceName)
+		val templateFile: File = new File(rootPath, resourceName)
 
 		try {
 			new BufferedInputStream(new FileInputStream(templateFile))
-		}
-		catch {
+		} catch {
 			case e: FileNotFoundException =>
 				Logger.error("Resource file is not found", e)
 				null
