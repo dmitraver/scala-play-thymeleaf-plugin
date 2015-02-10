5038b6f82cba645e58dc36e871459f0b25df8736
Dmitry Avershin <dmitraver@gmail.com>
2015-02-11 00:40:47 +0600

    added form validation capabilities for register template

--- sample/app/controllers/RegistrationController.scala
+++ sample/app/controllers/RegistrationController.scala
@@ -12,10 +12,20 @@ object RegistrationController extends Controller {
 	private val registrationForm = Form[User] (
 		mapping("firstName" -> nonEmptyText,
 		        "lastName" -> nonEmptyText,
-		        "age" -> number(min = 10, max = 100))(User.apply)(User.unapply)
+		        "age" -> number.verifying("Age must be between 10 and 100 years", age => age >= 10 && age <= 100))(User.apply)(User.unapply)
 	)
 
 	def register() = Action { implicit request =>
 		Ok(Thymeleaf.render("register", Map("userForm" -> FormWrapper(registrationForm))))
 	}
+
+	def userProfile() = Action { implicit request =>
+		val form = registrationForm.bindFromRequest
+
+		form.fold(
+			formWithErrors => BadRequest(Thymeleaf.render("register", Map("userForm" -> FormWrapper(formWithErrors)))),
+		  user => Ok(Thymeleaf.render("userprofile", Map("user" -> user)))
+		)
+	}
+
 }
--- sample/app/model/User.scala
+++ sample/app/model/User.scala
@@ -1,3 +1,8 @@
 package model
 
-case class User(firstName: String, lastName: String, age: Int)
+case class User(firstName: String, lastName: String, age: Int) {
+
+	def getFirstName = firstName
+	def getLastName = lastName
+	def getAge = age
+}
